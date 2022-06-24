d636ee1fef88aac38aa2ed80946ded099a48410b
Mitchell Tol <mitchell.tol@student.uva.nl>
2022-06-24 22:54:38 +0200

    added jsdoc to home_controller.js

--- src/assets/js/controllers/home_controller.js
+++ src/assets/js/controllers/home_controller.js
@@ -1,13 +1,24 @@
 import { Controller } from "./controller.js";
 
+/**
+ * Controls the home page.
+ * @author Mitchell Tol
+ */
 export class HomeController extends Controller {
     #view;
 
+    /**
+     * constructor
+     * @param { boolean } firstTimeSetup Set to true if this is the first time the website loads, so the nav and footer only load in once.
+     */
     constructor(firstTimeSetup) {
         super(firstTimeSetup);
         this.#initializeView();
     }
 
+    /**
+     * Initializes the main.
+     */
     async #initializeView() {
         this.#view = await (super.getHtmlFromFile("./src/html/pages/welcome.html"));
         super.setMain(this.#view);
