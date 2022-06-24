2a0a3cac8dad9c6b1267befae3fd13ff0496ab49
Mitchell Tol <mitchell.tol@student.uva.nl>
2022-06-24 22:53:20 +0200

    jsdoc toegevoegd aan controller.js

--- src/assets/js/controllers/controller.js
+++ src/assets/js/controllers/controller.js
@@ -1,14 +1,26 @@
 import { App } from "../app.js";
 
+/**
+ * Used for controlling the website.
+ * @author Mitchell Tol
+ */
 export class Controller {
     #navbar;
     #main;
     #footer;
 
+    /**
+     * constructor
+     * @param { boolean } firstTimeSetup Set to true if this is the first time the website loads, so the nav and footer only load in once.
+     */
     constructor(firstTimeSetup) {
         this.#initView(firstTimeSetup);
     }
 
+    /**
+     * Initializes the view.
+     * @param { boolean } firstTimeSetup Set to true if this is the first time the website loads, so the nav and footer only load in once.
+     */
     async #initView(firstTimeSetup) {
         this.#initMain();
         if (firstTimeSetup) {
@@ -21,6 +33,9 @@ export class Controller {
         }
     }
 
+    /**
+     * Initializes the navbar.
+     */
     async #initNavbar() {
         this.#navbar = await this.getHtmlFromFile("./src/html/elements/global/navbar.html");
         this.#navbar.querySelectorAll(".nav-link").forEach(link => {
@@ -30,18 +45,33 @@ export class Controller {
         });
     }
 
+    /**
+     * Initializes the footer.
+     */
     async #initFooter() {
         this.#footer = await this.getHtmlFromFile("./src/html/elements/global/footer.html");
     }
 
+    /**
+     * Initializes the main. But is to be set by setMain().
+     */
     #initMain() {
         this.#main = document.createElement("main");
     }
 
+    /**
+     * Sets the main to the given element.
+     * @param { HTMLElement } element The element to set as the main.
+     */
     setMain(element) {
         this.#main.innerHTML = element.innerHTML;
     }
 
+    /**
+     * Converts an HTML file to an HTML element.
+     * @param { string } filepath The path to the file to convert.
+     * @returns { HTMLElement }
+     */
     async getHtmlFromFile(filepath) {
         const dummyElement = document.createElement("div");
         const htmlString = await (await fetch(filepath)).text();
