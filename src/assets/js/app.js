8a97066ab8eaa5cb230e20ded99360cb16a41f7f
Mitchell Tol <mitchell.tol@student.uva.nl>
2022-06-24 23:33:08 +0200

    links without attached controller don't take action anymore

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -20,35 +20,36 @@ export class App {
      * @param { boolean } firstTimeSetup Set to true if this is the first time the website loads, so the nav and footer only load in once.
      */
     static loadPage(name, firstTimeSetup) {
-        // Load in the correct stylesheet.
-        document.querySelector(".page-stylesheet").innerHTML = null;
-        const stylesheet = document.createElement("link");
-        stylesheet.setAttribute("rel", "stylesheet");
-        stylesheet.setAttribute("href", `./src/assets/css/pages/${name}.css`);
-        document.querySelector(".page-stylesheet").append(stylesheet);
-
         // Load in controller
-        App.#loadController(name, firstTimeSetup);
+        if (App.#loadController(name, firstTimeSetup)) {
+            // Load in the correct stylesheet if loading the controller was succesful.
+            document.querySelector(".page-stylesheet").innerHTML = null;
+            const stylesheet = document.createElement("link");
+            stylesheet.setAttribute("rel", "stylesheet");
+            stylesheet.setAttribute("href", `./src/assets/css/pages/${name}.css`);
+            document.querySelector(".page-stylesheet").append(stylesheet);
+        }
     }
 
     /**
      * Loads a controller.
      * @param { string } name Name of the controller to load (lowercase).
      * @param { boolean } firstTimeSetup Set to true if this is the first time the website loads, so the nav and footer only load in once.
+     * @return { boolean } The success of loading a controller.
      */
     static #loadController(name, firstTimeSetup) {
         switch (name) {
             case "home":
                 new HomeController(firstTimeSetup);
-                break;
+                return true;
 
             case "projects":
                 new ProjectsController();
-                break;
+                return true;
 
             default:
-                new HomeController(firstTimeSetup);
-                break;
+                console.error("No controller was loaded.");
+                return false;
         }
     }
 }
